580b8da3ef8c78920fe9f07e190d22fc6a7db93b
Henry Sanchez <hsanchez@healthesystems.com>
2018-02-05 15:05:35 -0500

    added reducer.

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,7 +8,7 @@ export const FETCH_WEATER = 'FETCH_WEATHER';
 export function fetchWeather(city) {
     const url = `${ROOT_URL}&q=${city},us`;
     const request = axios.get(url);
-    
+
     return {
         type: FETCH_WEATER,
         payload: request
