6de9b01fb763c42b4f510f3ec3f0f969695073d1
Henry Sanchez <hsanchez@healthesystems.com>
2018-02-05 11:51:38 -0500

    create states for the control component. Added change handler on the input.

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,11 +1,30 @@
 import React, {Component} from 'react';
 
 export default class SearchBar extends Component {
+    
+    // create states for the control component
+    constructor(props) {
+        super(props);
+
+        this.state = { term: '' };
+
+        this.onInputChange = this.onInputChange.bind(this);
+    }
+
+    onInputChange(event) {
+        console.log(event.target.value);
+        this.setState({ term: event.target.value });
+    }
 
     render() {
         return (
             <form className="input-group">
-                <input />
+                {/* update state by using a change handler on the input */}
+                <input 
+                    placeholder="get a five day forcast in your favorite city"
+                    className="form-control"
+                    value={this.state.term}
+                    onChange={this.onInputChange} />
                 <span className="input-group-btn">
                     <button type="submit" className="btn btn-secondary">Submit</button>
                 </span>
